2d94c58b540f3300944b1eb4817d22c15336ff2c
Super Production <yokten10@gmail.com>
2022-01-06 11:32:36 +0700

    Initial Commit

---
+++ Character App/Character_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Character_AppApp.swift
+//  Character App
+//
+//  Created by Yok on 6/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Character_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Character App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Character App
+//
+//  Created by Yok on 6/1/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
